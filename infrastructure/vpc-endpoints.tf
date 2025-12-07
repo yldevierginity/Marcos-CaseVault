b113e65554c2d78bd944b2dd5417e6f33e12957f
0xshr00msz <yldeviermagpusao@gmail.com>
2025-12-08 06:52:49 +0800

    fix: add VPC endpoint for Secrets Manager to resolve Lambda 502 errors

---
+++ infrastructure/vpc-endpoints.tf
@@ -0,0 +1,36 @@
+# VPC Endpoints for AWS Services
+resource "aws_vpc_endpoint" "secretsmanager" {
+  vpc_id              = aws_vpc.main.id
+  service_name        = "com.amazonaws.${var.aws_region}.secretsmanager"
+  vpc_endpoint_type   = "Interface"
+  subnet_ids          = aws_subnet.private[*].id
+  security_group_ids  = [aws_security_group.vpc_endpoints.id]
+  private_dns_enabled = true
+
+  tags = merge(local.common_tags, {
+    Name = "${var.project_name}-secretsmanager-endpoint"
+  })
+}
+
+resource "aws_security_group" "vpc_endpoints" {
+  name_prefix = "${var.project_name}-vpc-endpoints-sg"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 443
+    to_port         = 443
+    protocol        = "tcp"
+    security_groups = [aws_security_group.lambda.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = merge(local.common_tags, {
+    Name = "${var.project_name}-vpc-endpoints-sg"
+  })
+}
