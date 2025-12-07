c9eb9107b3db3f350e082e3dc6686b3d9d2078da
0xshr00msz <yldeviermagpusao@gmail.com>
2025-12-08 07:16:40 +0800

    fix: VPC endpoint security group use CIDR instead of security group reference

--- infrastructure/vpc-endpoints.tf
+++ infrastructure/vpc-endpoints.tf
@@ -17,10 +17,10 @@ resource "aws_security_group" "vpc_endpoints" {
   vpc_id      = aws_vpc.main.id
 
   ingress {
-    from_port       = 443
-    to_port         = 443
-    protocol        = "tcp"
-    security_groups = [aws_security_group.lambda.id]
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = [aws_vpc.main.cidr_block]
   }
 
   egress {
