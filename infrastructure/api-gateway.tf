a16adb933d66bb31b40102d2a697a587c608c17f
0xshr00msz <yldeviermagpusao@gmail.com>
2025-12-08 04:47:16 +0800

    fix: force API Gateway redeployment for CORS changes

--- infrastructure/api-gateway.tf
+++ infrastructure/api-gateway.tf
@@ -186,6 +186,13 @@ resource "aws_api_gateway_deployment" "main" {
 
   rest_api_id = aws_api_gateway_rest_api.main.id
 
+  triggers = {
+    redeployment = sha1(jsonencode([
+      aws_api_gateway_integration_response.clients_options.id,
+      aws_api_gateway_integration_response.cases_options.id,
+    ]))
+  }
+
   lifecycle {
     create_before_destroy = true
   }
