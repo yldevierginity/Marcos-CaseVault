324c3567f4405473b5b2c9394ad8f9e97b359bc1
0xshr00msz <yldeviermagpusao@gmail.com>
2025-12-10 20:11:15 +0800

    feat(infra): migrate RDS from MySQL to PostgreSQL 16.3
    - Change database engine from MySQL 8.0 to PostgreSQL 16.3
    - Update port from 3306 to 5432
    - Configure database name as casevault_db
    - Set username as casevault_admin
    - Use gp3 storage type for better performance
    
    BREAKING CHANGE: Database engine changed from MySQL to PostgreSQL

---
+++ Infrastructure/4-rds.tf
@@ -0,0 +1,77 @@
+resource "random_password" "db_password" {
+  length  = 16
+  special = true
+}
+
+resource "aws_secretsmanager_secret" "db_credentials" {
+  name = "${var.project_name}-db-credentials"
+}
+
+resource "aws_secretsmanager_secret_version" "db_credentials" {
+  secret_id = aws_secretsmanager_secret.db_credentials.id
+  secret_string = jsonencode({
+    username = "casevault_admin"
+    password = random_password.db_password.result
+    engine   = "postgres"
+    host     = aws_db_instance.postgres.address
+    port     = 5432
+    dbname   = "casevault_db"
+  })
+}
+
+resource "aws_db_subnet_group" "default" {
+  name       = "${var.project_name}-sn-group"
+  subnet_ids = [module.vpc.intra_subnets[0], module.vpc.intra_subnets[1]]
+
+  tags = {
+    Name = "${var.project_name}-sn-group"
+  }
+}
+
+resource "aws_security_group" "rds" {
+  name        = "${var.project_name}-sg-rds"
+  vpc_id      = module.vpc.vpc_id
+
+  ingress {
+    from_port       = 5432
+    to_port         = 5432
+    protocol        = "tcp"
+    security_groups = [aws_security_group.web.id, aws_security_group.bastion.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.project_name}-sg-rds"
+  }
+}
+
+resource "aws_db_instance" "postgres" {
+  identifier              = "${var.project_name}-db"
+  allocated_storage       = 20
+  storage_type            = "gp3"
+  engine                  = "postgres"
+  engine_version          = "16.3"
+  instance_class          = "db.t3.micro"
+  db_name                 = "casevault_db"
+  username                = "casevault_admin"
+  password                = random_password.db_password.result
+  db_subnet_group_name    = aws_db_subnet_group.default.name
+  vpc_security_group_ids  = [aws_security_group.rds.id]
+  skip_final_snapshot     = true
+  publicly_accessible     = false
+
+  tags = {
+    Name        = "${var.project_name}-db"
+    Environment = "production"
+  }
+}
+
+output "db_secret_arn" {
+  value = aws_secretsmanager_secret.db_credentials.arn
+}
